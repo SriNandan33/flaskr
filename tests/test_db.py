fbf4e42a92a5bd3d0e0c3f49eaf333a640c9d602
srinivas33 <msrinivasa@edyst.com>
2018-12-24 19:43:23 +0530

    test cli command

--- tests/test_db.py
+++ tests/test_db.py
@@ -12,3 +12,15 @@ def test_get_close_db(app):
         db.execute('SELECT 1')
     
     assert 'closed' in str(e)
+
+def test_init_db_command(runner, monkypatch):
+    class Recorder(object):
+        called = False
+    
+    def fake_init_db():
+        Recorder.called = True
+
+    monkypatch.setattr('flaskr.db.init_db', fake_init_db)
+    result = runner.invoke(args=['init-db'])
+    assert 'Initialized' in result.output
+    assert Recorder.called
